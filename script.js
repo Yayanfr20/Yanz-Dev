e85c518b1d53aaa49e977ad52039cc0ae0f73c21
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-08-23 12:06:12 +0700

    up

---
+++ script.js
@@ -0,0 +1,10 @@
+// sidebar
+function sidebar() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.setAttribute("style", "");
+}
+
+function closeSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.setAttribute("style", "display:none;");
+}
